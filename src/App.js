dccce1736123bc6e954ca608816f6b19a6eba46c
saska9 <abrankovic862@gmail.com>
2023-09-02 19:16:42 +0200

    zavrsena aplikacija

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Body from './components/Body';
+import FavoritesBody from './components/FavoritesBody';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {useState} from 'react'
 
 function App() {
+
+  const [products] = useState([
+    {
+      id: 1,
+      img: "https://www.stefanovic1886.com/wp-content/uploads/2019/06/NM039ID0200W.jpg",
+      name: "Ogrlica - dijamant",
+      price: 38000,
+      favorite : false
+    },
+    {
+      id: 2,
+      img: "https://www.moonbeamshop.com/wp-content/uploads/2020/11/Angel-gold-scaled.jpg",
+      name: "Ogrilca Angel - zlato",
+      price: 21000,
+      favorite : false
+    },
+    {
+      id: 3,
+      img: "https://www.zlatarafiera.rs/wp-content/uploads/2019/05/VP-300-22750-BZ.jpg",
+      name: "Prsten - dijamant",
+      price: 34000,
+      favorite : false
+    },
+    {
+      id: 4,
+      img: "https://www.zlatarapanic.rs/images/stories/virtuemart/product/zlatne-narukvice26.jpg",
+      name: "Narukvica - zlato",
+      price: 12000,
+      favorite : false
+    },
+    {
+      id: 5,
+      img: "https://www.pandorashop.rs/files/thumbs/files/images/slike_proizvoda/thumbs_350/592548C01_350_350px.jpg.webp",
+      name: "Narukvica - belo zlato",
+      price: 16000,
+      favorite : false
+    },
+    {
+      id: 6,
+      img: "https://img5.lalafo.com/i/posters/original/35/db/a2/eb5ff51d13cdabe2b209e57be4.jpeg",
+      name: "Nanogica - srebro",
+      price: 7400,
+      favorite : false
+    }
+  ])
+
+  
+  const [numFav, setNumFav]=useState(0)
+  const [favProd, setFavProd]=useState([])
+  
+  const addFavorites = (id) =>{
+    setNumFav(numFav+1)
+    products.map((product)=>{
+      if(product.id === id){
+        product.favorite=true
+      }
+    })
+    refreshFavorites()
+  }
+
+  const removeFavorite = (id) => {
+    if(numFav>0){
+      setNumFav(numFav-1)
+    }
+    products.map((product)=>{
+      if(product.id === id){
+        product.favorite=false
+      }
+    })
+    refreshFavorites()
+  }
+
+  const refreshFavorites = () => {
+    var newFav=products.filter((product)=>product.favorite===true)
+    setFavProd(newFav)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar numFavorite={numFav}></Navbar>
+        <Routes>
+          <Route path='/' element={
+            <Body/>
+          }/>
+          <Route path='/products' element={
+            <Products products={products} addFavorites={addFavorites} removeFavorite={removeFavorite}/>
+          }/>
+          <Route path='/favorites' element={
+            <FavoritesBody products={favProd} removeFavorite={removeFavorite}/>
+          }/>
+        </Routes>
+      <Footer></Footer>
+    </BrowserRouter> 
+    </>  
   );
 }
+
+
 
 export default App;
